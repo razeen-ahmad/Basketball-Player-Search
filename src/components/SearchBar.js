e47dc2233326ce5c1b8eba400556970b700446ea
Razeen Ahmad <rahmad875@gmail.com>
2021-04-04 02:39:03 -0500

    refactored bball search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Container, FormControl } from "@chakra-ui/react"
 
-class SearchBar extends React.Component {
+const SearchBar = ({ onSearch }) => {
 
-    state = { searchTerm: '' }
+    const[searchTerm, setSearchTerm] = useState("");
 
-    onChange = (event) => {
+    const onChange = (event) => {
         event.preventDefault();
     }
 
-    onSubmit = (event) => {
+    const onSubmit = (event) => {
         event.preventDefault();
-        this.props.onSearch(this.state.searchTerm);
+        onSearch(searchTerm);
     }
 
-    render() {
-        return (
-            <Container maxWidth="100ch" mt={50}>
-                <form onChange={this.onChange} onSubmit={this.onSubmit}>
-                    <FormControl id="search bar">
-                        <Input 
-                            type="text"
-                            placeholder="Search..." 
-                            size="lg" 
-                            variant="flushed" 
-                            value={this.state.searchTerm}
-                            onChange={(e) => this.setState({ searchTerm: e.target.value })}
-                        />
-                    </FormControl>
-                </form>
-            </Container>
-        )
-    }
+    return (
+        <Container maxWidth="100ch" mt={50}>
+            <form onChange={onChange} onSubmit={onSubmit}>
+                <FormControl id="search bar">
+                    <Input 
+                        type="text"
+                        placeholder="Search..." 
+                        size="lg" 
+                        variant="flushed" 
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </FormControl>
+            </form>
+        </Container>
+    )
 };
 
 export default SearchBar;
--- src/pages/bball-search.js
+++ src/pages/bball-search.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import UseGames from '../hooks/UseGames';
 import SearchBar from '../components/SearchBar';
@@ -8,21 +8,22 @@ import GameList from '../components/GameList';
 import balldontlie from '../api/balldontlie';
 
 
-class BballSearch extends React.Component {
-    state = {
-        games: []
-    };
-    getData = async() => {
+const BballSearch = () =>  {
+
+    const [games, setGames] = useState([]);
+
+    const getData = async() => {
         const response = await balldontlie.get('/games');
-        this.setState({ games: response.data.data });
-    };
-    componentDidMount() {
-        this.getData();
+        setGames(response.data.data);
     };
 
-    onSearch = async season => {
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const onSearch = async season => {
         if(season.length === 0){
-            this.getData();
+            getData();
         }
         else {
             const response = await balldontlie.get('/games', {
@@ -30,26 +31,24 @@ class BballSearch extends React.Component {
                     "seasons[]": season
                 }
             });
-            this.setState({ games: response.data.data });
+            setGames(response.data.data);
         }
     };
     
     
-    render() {
-        return (
-            <Layout>
-                <Head>
-                    <title>Season Search</title>
-                </Head>
-                
-                {/*search bar*/}
-                <SearchBar onSearch={this.onSearch}/>
+    return (
+        <Layout>
+            <Head>
+                <title>Season Search</title>
+            </Head>
+            
+            {/*search bar*/}
+            <SearchBar onSearch={onSearch}/>
 
-                {/* render results from search bar */}
-                <GameList games={this.state.games}/>
-            </Layout>
-        );
-    };
+            {/* render results from search bar */}
+            <GameList games={games}/>
+        </Layout>
+    );
 };
 
 export default BballSearch;
