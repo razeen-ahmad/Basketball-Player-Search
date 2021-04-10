52f7cb3483c7c7c5c90de5cd69d70a868f5b67ec
Razeen Ahmad <rahmad875@gmail.com>
2021-04-10 16:29:27 -0500

    added home page image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout';
+import { Box, Text, Image, HStack, Center } from '@chakra-ui/react';
+// import Image from 'next/image';
 
 export default function Home({ properties }) {
   //properties === all data in 'player_clicks' collection
@@ -9,6 +11,22 @@ export default function Home({ properties }) {
       <Head>
         <title>Basketball Player Search</title>
       </Head>
+      <Center>
+        <Box justifyContent="center" boxSize="xl">
+          <Image 
+            src="/bballcourt.jpg" 
+            objectFit="fill"
+            alt="Basketball Court with Sunset" />
+        </Box>
+      </Center>
+      {/* <Box objectFit="contain">
+        <Image 
+          src="/bballcourt.jpg"
+          height={20}
+          width={50}
+          layout="responsive"
+        />
+      </Box> */}
       <Link href='/player-search'>
         <a>To player search!</a>
       </Link>
